de17734ca80e35690a87f8b2259adae14d7a33ff
Tickether <tickether@gmail.com>
2025-05-15 20:46:15 +0000

    feat: add fc frame embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,10 +20,27 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const frame = {
+    version: "next",
+    imageUrl: "https://warp.3wb.club/opengraph-image.png",
+    button: {
+      title: "Finance a 3-Wheeler 🛺💨",
+      action: {
+        type: "launch_frame",
+        url: "https://warp.3wb.club",
+        name:"3WB P2P Fleet Financing",
+        splashImageUrl: "https://warp.3wb.club/3wbClubLogo.png",
+        splashBackgroundColor:"#f5f0ec"
+      }
+    }
+  }
   return (
     <html lang="en">
       <head>
-        <meta property="og:image" content="https://warp.3wb.club/opengraph-image.png" />
+        <meta 
+          name="fc:frame"
+          content={JSON.stringify(frame)} 
+        />
       </head>
       <body
         className={`${geistMono.className}`}
