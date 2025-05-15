84ab7ba1650fcbc8963db64874f30810f4345f5b
Tickether <tickether@gmail.com>
2025-05-15 23:55:56 +0000

    feat: send transaction

--- hooks/useDivvi.tsx
+++ hooks/useDivvi.tsx
@@ -3,11 +3,14 @@ import { getDataSuffix, submitReferral } from "@divvi/referral-sdk"
 import { useState } from "react"
 import { createWalletClient, encodeFunctionData, erc20Abi, http, maxUint256 } from "viem"
 import { celo } from "viem/chains"
+import { useSendTransaction } from "wagmi"
+
 
 export const useDivvi = () => {
   
     const [loading, setLoading] = useState(false)
-  
+    const { data: txHash, sendTransactionAsync } = useSendTransaction()
+
     async function registerUser(account: `0x${string}`, to: `0x${string}`) {
       try {
         setLoading(true)
@@ -31,14 +34,16 @@ export const useDivvi = () => {
           consumer: "0x99342D3CE2d10C34b7d20D960EA75bd742aec468",
           providers: ["0x5f0a55FaD9424ac99429f635dfb9bF20c3360Ab8", "0xB06a1b291863f923E7417E9F302e2a84018c33C5", "0x6226ddE08402642964f9A6de844ea3116F0dFc7e"],
         })
-        const txHash = await walletClient.sendTransaction({
-          account,
+
+        
+        await sendTransactionAsync({
           to: to,
           data: data + dataSuffix as `0x${string}`,
           value: BigInt(0),
-          // ... other transaction parameters
+          chainId: celo.id
         })
-
+        
+        
         // Step 3: Get the chain ID of the chain that the transaction was sent to
         const chainId = await walletClient.getChainId()
 
