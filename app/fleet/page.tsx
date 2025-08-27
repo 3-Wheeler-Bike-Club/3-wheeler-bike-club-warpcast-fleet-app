229bf97c9a4048829c39a82bce4f355a6d9f6b70
Tickether <tickether@gmail.com>
2025-08-27 03:43:28 +0000

    fix: Use min-h-screen for Fleet page container
    Replaces h-screen with min-h-screen on the Fleet page container to ensure the content stretches to at least the viewport height but can grow if needed.

--- app/fleet/page.tsx
+++ app/fleet/page.tsx
@@ -3,7 +3,7 @@ import { Wrapper } from "@/components/fleet/wrapper";
 export default function Fleet() {
 
   return (
-    <div className="flex flex-col w-full h-screen">
+    <div className="flex flex-col w-full min-h-screen">
       <Wrapper/>
     </div>
   );
